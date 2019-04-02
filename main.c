0f05ad4bdf208815ce24d7fbfd5d025bf0dac353
William Cranney <cranneywj@gmail.com>
2019-04-02 02:32:53 +0000

    Lab Ending Commit

--- main.c
+++ main.c
@@ -11,8 +11,9 @@ int main()
 {
 char c;
 
-for(c = 'A'; c <= 'Z'; ++c)
-    printf("%c", c);
+
+for(c = 'A'; c <= 'Z'; ++c) // cycle through the alphabet until creaches the value of Z
+    printf("%c", c); // print each value of the alphabet as c cycles through each letter
 
 return  0;
 }
