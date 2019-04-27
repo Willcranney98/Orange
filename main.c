4b88783e41ac9d4d5b8a6925b5ab39eb1b2db3fc
William Cranney <cranneywj@gmail.com>
2019-04-27 01:06:07 +0000

    Getting started finally

--- main.c
+++ main.c
@@ -3,12 +3,20 @@
 int main()
 {
 char c;
-int key = 3
-string inout = ""
+int dif = 0;
+int count = 0;
+char input[6] = {'A','B','C','D'};
 
 for(c = 'A'; c <= 'Z'; ++c) // cycle through the alphabet until creaches the value of Z
     printf("%c", c); // print each value of the alphabet as c cycles through each letter
-
+    if(input[0]='A'){
+        int output = count + dif;
+        c = c+output;
+        printf("%c", c);
+        c='Z';
+        
+    }
+    count++;
 return  0;
 }
 
