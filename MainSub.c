809b8caad15cf9d547e4d8528c1125b81d6244e7
William Cranney <cranneywj@gmail.com>
2019-04-28 09:50:46 +0000

    Comments need fixing

--- MainSub.c
+++ MainSub.c
@@ -2,36 +2,36 @@
 
 int main(void)
 {
-   int alpha[26];
+   int alpha[26];//array size 26 for alphabet letters
    char x;
    char y;
-   FILE *sub;
-sub = fopen("sub.txt", "r");
+   FILE *sub;//initialises sub file
+sub = fopen("sub.txt", "r");//open sub file
 
 for(int i=0; i<26; i++){
     
-     fscanf(sub, "%c", &x);
-     alpha[i] = x;
-     //printf("%c", alpha[i]);
+     fscanf(sub, "%c", &x);//assigns sub file to x
+     alpha[i] = x;//x assigned to array for alphabet
 }
 
 
-  FILE *message;
-message = fopen("message.txt", "r");
+  FILE *message;//initalise message file
+message = fopen("message.txt", "r");//open message file
    while(!feof(message)) {
-       fscanf(message, "%c", &y);
-       if(y==32){printf("%c",y);}else{
-       for(int i=0; i<26; i++){
-           if((alpha[i]==y)){
+       fscanf(message, "%c", &y);//assigns message file to y
+       if(y==32){
+           printf("%c",y);
+           }
+       else{
+           for(int i=0; i<26; i++){//test other values
+               if((alpha[i]==y)){//assign sub to message
                
-               printf("%c", i+65);
+               printf("%c", i+65);//print decrypted substitution
            }
            
            
        }
        }
-       
-       // commit not working pls work
        
    }
 }
