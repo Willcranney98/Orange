da44357722e2aeb8d87149e3be831d445c8288da
William Cranney <cranneywj@gmail.com>
2019-04-28 09:01:36 +0000

    Substitution working for given substitution

--- MainSub.c
+++ MainSub.c
@@ -4,14 +4,34 @@ int main(void)
 {
    int alpha[26];
    char x;
-   FILE *text;
-text = fopen("sub.txt", "r");
+   char y;
+   FILE *sub;
+sub = fopen("sub.txt", "r");
 
 for(int i=0; i<26; i++){
     
-     fscanf(text, "%c", &x);
+     fscanf(sub, "%c", &x);
      alpha[i] = x;
-     printf("%c", x);
+     //printf("%c", alpha[i]);
 }
-   
+
+
+  FILE *message;
+message = fopen("message.txt", "r");
+   while(!feof(message)) {
+       fscanf(message, "%c", &y);
+       if(y==32){printf("%c",y);}else{
+       for(int i=0; i<26; i++){
+           if((alpha[i]==y)){
+               
+               printf("%c", i+65);
+           }
+           
+           
+       }
+       }
+       
+       
+       
+   }
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main()
 
 //int asc;
 //int variable;
-int dif = 12; //key
+int dif = 19; //key
 //char str1[20];
 int count = 7; 
 //char input[90];
@@ -35,37 +35,12 @@ if(x>64){
     }
 }
       printf("%c", x);
-    //printf("%d", x);
-    //printf("%c", " ");
 
-    //printf('\n');
- 
-    //printf("%c", x);
-   
-    
-    
-    
-    /*for(c = 'A'; c <= 'Z'; ++c){ // cycle through the alphabet until creaches the value of Z
-    
-    if((c='x')){
-        int output = (count + dif)%26;
-        c = c+output;
-        printf("%c", c);
-        c='Z';
-        
-    }
-}*/
 
     count++;
     
     
 }
-//char str[1024];
-//scanf("%s", str);
-//printf("Read: %s\n", str);
-//printf("Enter name: ");
-//printf("Enter a number: ");
-//scanf("%d", &variable);
 
     
 return  0;
